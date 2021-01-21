091c2230dd6b8c181d43ac6fd6f65a8a1f546127
Jerry Chang <therealjerrycan@gmail.com>
2021-01-21 01:53:29 -0800

    Remove mock data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,48 +3,14 @@ import {
 } from 'next'
 import axios from 'axios'
 import Head from 'next/head'
+import styled from 'styled-components'
 
 import {
     Button,
     Box,
     Typography
 } from '@material-ui/core'
-
-import styled from 'styled-components'
-
 import { MovieList } from '../components/movies'
-
-
-const movies = [{
-    title: 'Movie Title 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lacinia turpis. Donec et malesuada lectus. Duis elementum elit vitae rutrum consequat. Aenean pulvinar posuere dapibus. Proin nec lacus sit amet tortor ullamcorper faucibus. Nam faucibus a erat vel tempor. Fusce nec enim ex. Nullam dui orci, suscipit sed blandit ac, tristique in est. Maecenas finibus tellus lorem, id molestie ante iaculis interdum.',
-    imageUrl: '/images/mock-image.jpg'
-},
-    {
-
-    title: 'Movie Title 2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lacinia turpis. Donec et malesuada lectus. Duis elementum elit vitae rutrum consequat. Aenean pulvinar posuere dapibus. Proin nec lacus sit amet tortor ullamcorper faucibus. Nam faucibus a erat vel tempor. Fusce nec enim ex. Nullam dui orci, suscipit sed blandit ac, tristique in est. Maecenas finibus tellus lorem, id molestie ante iaculis interdum.',
-    imageUrl: '/images/mock-image.jpg'
-    },
-    {
-
-    title: 'Movie Title 3',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lacinia turpis. Donec et malesuada lectus. Duis elementum elit vitae rutrum consequat. Aenean pulvinar posuere dapibus. Proin nec lacus sit amet tortor ullamcorper faucibus. Nam faucibus a erat vel tempor. Fusce nec enim ex. Nullam dui orci, suscipit sed blandit ac, tristique in est. Maecenas finibus tellus lorem, id molestie ante iaculis interdum.',
-    imageUrl: '/images/mock-image.jpg'
-    },
-    {
-
-    title: 'Movie Title 4',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lacinia turpis. Donec et malesuada lectus. Duis elementum elit vitae rutrum consequat. Aenean pulvinar posuere dapibus. Proin nec lacus sit amet tortor ullamcorper faucibus. Nam faucibus a erat vel tempor. Fusce nec enim ex. Nullam dui orci, suscipit sed blandit ac, tristique in est. Maecenas finibus tellus lorem, id molestie ante iaculis interdum.',
-    imageUrl: '/images/mock-image.jpg'
-    },
-    {
-
-    title: 'Movie Title 5',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lacinia turpis. Donec et malesuada lectus. Duis elementum elit vitae rutrum consequat. Aenean pulvinar posuere dapibus. Proin nec lacus sit amet tortor ullamcorper faucibus. Nam faucibus a erat vel tempor. Fusce nec enim ex. Nullam dui orci, suscipit sed blandit ac, tristique in est. Maecenas finibus tellus lorem, id molestie ante iaculis interdum.',
-    imageUrl: '/images/mock-image.jpg'
-    }
-]
 
 interface HomeProps {
     latestMovies: any;
@@ -74,10 +40,10 @@ const Home = (props: HomeProps) => {
 Home.getInitialProps = async(
     ctx: NextPageContext
 ) => {
-    const { data: { results } } = await axios.get('http://localhost:3000/api/movies')
+    const {
+        data: { results }
+    } = await axios.get('http://localhost:3000/api/movies')
     return { latestMovies: results }
 }
-
-
 
 export default Home
